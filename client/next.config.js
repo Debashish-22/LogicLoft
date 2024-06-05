d0a73ffb5963e3148923f3be34aa2649d88c998c
Debashish jena <debashishjena246@gmail.com>
2024-06-05 21:57:31 +0530

    client updated

--- client/next.config.js
+++ client/next.config.js
@@ -5,7 +5,7 @@ module.exports = {
         AUTH_TOKEN: 'APTKN',
         DEVICE_TOKEN: 'APDID',
         GOOGLE_ROOT_URI: 'https://accounts.google.com/o/oauth2/v2/auth',
-        GOOGLE_OAUTH_REDIRECT: 'http://localhost:8000/api/auth/google-login',
+        GOOGLE_OAUTH_REDIRECT: 'http://13.234.204.55:8000/api/auth/google-login',
         GOOGLE_OAUTH_CLIENT_ID: '385937620657-9c8ldbh0igs9vu7kveus75j9kuql2ooe.apps.googleusercontent.com',
     },
     reactStrictMode: false,
